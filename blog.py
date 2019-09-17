4d41d6dfec970a9be09865faffa7c22ad9bf3b30
Hbehrainwala <hbehrainwala@gmail.com>
2019-09-17 10:28:46 +0530

    design login and register page

--- blog.py
+++ blog.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '170768a769713cc5414600cff104459c'
 
 posts = [
 	{
@@ -23,7 +27,26 @@ def home():
 
 @app.route("/about")
 def about():
-	return render_template('about.html', title='about')
+	return render_template('about.html', title='About')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		flash(f'Account created for {form.username.data}!', 'success')
+		return redirect(url_for('home'))
+	return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		if form.email.data == 'admin@example.com' and form.password.data == 'admin12345':
+			flash(f'You have been logged in!', 'success')
+			return redirect(url_for('home'))
+		else:
+			flash(f'Login Unsuccessful. Please check username and password', 'danger')
+	return render_template('login.html', title='Login', form=form)
 
 if "__name__"=="__main__":
 	app.run(debug=True)
